64d7b124800dfccfd3f71b17164efc0bf7ddbc33
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-11-03 12:22:48 +0200

    Initial Commit

---
+++ test/clj_naive_bayes/t_core.clj
@@ -0,0 +1,15 @@
+(ns clj_naive_bayes.t-core
+  (:use midje.sweet)
+  (:use [clj_naive_bayes.core]))
+
+(facts "about `first-element`"
+  (fact "it normally returns the first element"
+    (first-element [1 2 3] :default) => 1
+    (first-element '(1 2 3) :default) => 1)
+
+  ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
+  (fact "default value is returned for empty sequences"
+    (first-element [] :default) => :default
+    (first-element '() :default) => :default
+    (first-element nil :default) => :default
+    (first-element (filter even? [1 3 5]) :default) => :default))
