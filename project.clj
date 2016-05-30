e92bae079fefa3dd7e3861f693a8488123207bc5
Nikos Fertakis <nikos@skroutz.gr>
2016-05-30 14:04:36 +0300

    Bump version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-naive-bayes "0.0.1-SNAPSHOT"
+(defproject clj-naive-bayes "0.1.0"
   :description "Naive bayes in Clojure!"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]
