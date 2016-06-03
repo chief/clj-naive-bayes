7a786298c5b0096e19123e984385f74bb0067366
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2016-06-03 12:11:00 +0300

    Upgrade some libs

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clj-naive-bayes "0.1.6"
   :description "Naive bayes in Clojure!"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.5.0"]
-                 [org.clojure/core.memoize "0.5.8"]
+                 [cheshire "5.6.1"]
+                 [org.clojure/core.memoize "0.5.9"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [com.stuartsierra/component "0.3.1"]
                  [org.clojure/data.csv "0.1.3"]
                  [spyscope "0.1.5"]
-                 [prismatic/schema "1.0.4"]]
+                 [prismatic/schema "1.1.1"]]
   :jvm-opts ["-Xmx4g"]
-  :plugins [[lein-marginalia "0.8.0"]
+  :plugins [[michaelblume/lein-marginalia "0.9.0"]
             [lein-cljfmt "0.5.3"]])
