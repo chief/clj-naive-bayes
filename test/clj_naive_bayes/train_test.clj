4562d2947c1ed946c9d3a05b2f0a7238ba5bd104
Nikos Fertakis <nikos@skroutz.gr>
2016-06-03 11:00:08 +0300

    Add test for multiple document occurences
    Since the training algorithm supports a column in the dataset that
    specifies occurences for each document (to avoid duplicate lines), we
    need to add a test for this behaviour.

--- test/clj_naive_bayes/train_test.clj
+++ test/clj_naive_bayes/train_test.clj
@@ -3,7 +3,7 @@
             [clj_naive_bayes.core :as core]
             [clojure.test :refer :all]))
 
-(deftest test-train
+(deftest test-train-multinomial
   (let [documents [["Chinese Beijing Chinese" "China"]
                    ["Chinese Chinese Shanghai" "China"]
                    ["Chinese Macao" "China"]
@@ -23,6 +23,27 @@
     (is (= (get-in classes ["China" :n]) 3))
     (is (= (get-in tokens ["chinese" "China"]) 5)))
 
+  (testing "when document occurrences are supplied"
+    (let [documents [["Chinese Beijing Chinese" "China" 2]
+                     ["Chinese Chinese Shanghai" "China" 1]
+                     ["Chinese Macao" "China" 3]
+                     ["Tokyo Japan Chinese" "Japan" 1]]
+          classifier (core/new-classifier)
+          do-train (train/train classifier documents)
+          data @(:data classifier)
+          all (:all data)
+          tokens (:tokens data)
+          classes (:classes data)]
+
+      (is (= (:n all) 7))
+      (is (= (:v all) 6))
+      (is (= (:st all) 18))
+      (is (= (get-in tokens ["chinese" :all]) 10))
+      (is (= ["chinese" "beijing" "shanghai" "macao" "tokyo" "japan"] (keys tokens)))
+      (is (= (get-in classes ["China" :n]) 6))
+      (is (= (get-in tokens ["chinese" "China"]) 9)))))
+
+(deftest test-train-multinomial-positional
   (let [documents [["Chinese Beijing Chinese" "China"]
                    ["Chinese Chinese Shanghai" "China"]
                    ["Chinese Macao" "China"]
@@ -40,8 +61,9 @@
     (is (= (get-in tokens ["chinese" :all 0]) 3))
     (is (= ["chinese" "beijing" "shanghai" "macao" "tokyo" "japan"] (keys tokens)))
     (is (= (get-in classes ["China" :n]) 3))
-    (is (= (get-in tokens ["chinese" "China" 1]) 1)))
+    (is (= (get-in tokens ["chinese" "China" 1]) 1))))
 
+(deftest test-train-binary-nb
   (let [documents [["Chinese Beijing Chinese" "China"]
                    ["Chinese Chinese Shanghai" "China"]
                    ["Chinese Macao" "China"]
